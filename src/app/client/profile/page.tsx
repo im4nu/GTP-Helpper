30b832db6e560165023afec01a0273dc3891fb40
im4nu <victorx811@gmail.com>
2023-07-28 10:53:39 -0300

    added logout function on profile screen

--- src/app/client/profile/page.tsx
+++ src/app/client/profile/page.tsx
@@ -5,7 +5,8 @@ import StackLayout from "@/layouts/Stack";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { userData } from "../../../../utils/user";
-import { Link } from "@/components/Link";
+import Link from "next/link";
+import { LogoutCurve } from "iconsax-react";
 
 export default function Profile() {
   const [firstTime, setFirstTime] = useState(true);
@@ -48,50 +49,74 @@ export default function Profile() {
           </div>
         </>
       ) : (
-        <div className="flex flex-row w-full items-center h-[calc(100vh-180px)] justify-around">
-          <div className="flex flex-col items-start w-1/3 gap-4">
-            <Image
-              src={userData.image}
-              width={150}
-              height={150}
-              alt="Imagem de perfil do usuário"
-            />
-            <div className="flex flex-col gap-2">
-              <p className="text-sm text-white-200">Meu nome:</p>
-              <p>{userData.name}</p>
-            </div>
-            <div className="flex flex-col gap-2">
-              <p className="text-sm text-white-200">Meu Email:</p>
-              <p>{userData.email}</p>
-            </div>
-            <div className="flex flex-col gap-2">
-              <p className="text-sm text-white-200">Meu Plano:</p>
-              <p>{userData.plan}</p>
-            </div>
-            <div className="flex flex-col gap-2">
-              <p className="text-sm text-white-200">Uso o GPT Helpper desde:</p>
-              <p>{userData.createdAt}</p>
+        <div className="flex flex-col w-full items-center h-[calc(100vh-180px)] justify-evenly">
+          <div className="flex flex-row w-full items-center justify-around">
+            <div className="flex flex-col items-start w-1/3 gap-4">
+              <Image
+                src={userData.image}
+                width={150}
+                height={150}
+                alt="Imagem de perfil do usuário"
+              />
+              <div className="flex flex-col gap-2">
+                <p className="text-sm text-white-200">Meu nome:</p>
+                <p>{userData.name}</p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-sm text-white-200">Meu Email:</p>
+                <p>{userData.email}</p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-sm text-white-200">Meu Plano:</p>
+                <p>{userData.plan}</p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-sm text-white-200">
+                  Uso o GPT Helpper desde:
+                </p>
+                <p>{userData.createdAt}</p>
+              </div>
+
+              <Button>Editar dados</Button>
             </div>
 
-            <Button>Editar dados</Button>
+            <div className="flex flex-col items-center w-1/3 gap-4">
+              <p className="text-2xl">Meus Créditos</p>
+              <Image
+                src={"/img/credits.png"}
+                width={300}
+                height={300}
+                alt="Imagem de créditos"
+              />
+
+              <p>
+                <span className="font-semibold">{userData.credits}</span>{" "}
+                créditos disponíveis
+              </p>
+
+              <Link variant={"containedGreen"} href={"/"}>
+                Adquirir mais créditos
+              </Link>
+            </div>
           </div>
 
-          <div className="flex flex-col items-center w-1/3 gap-4">
-            <p className="text-2xl">Meus Créditos</p>
-            <Image
-              src={"/img/credits.png"}
-              width={300}
-              height={300}
-              alt="Imagem de créditos"
-            />
+          <div className="flex w-full items-center justify-between">
+            <Link href={"#"} className="flex flex-row items-center gap-2">
+              <Image
+                alt="Imagem de de ajuda"
+                src={"/avatars/confuzed.png"}
+                width={50}
+                height={50}
+              />
+              <p className="text-xs underline">Precisa de ajuda?</p>
+            </Link>
 
-            <p>
-              <span className="font-semibold">{userData.credits}</span> créditos
-              disponíveis
-            </p>
-
-            <Link variant={"containedGreen"} href={"/"}>
-              Adquirir mais créditos
+            <Link
+              href={"/auth/login"}
+              className="flex flex-row items-center gap-2"
+            >
+              <LogoutCurve size="32" />
+              <p className="text-xs underline">Sair da sua conta</p>
             </Link>
           </div>
         </div>
