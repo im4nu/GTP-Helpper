a4050d7613a148bf1a888fdcf385eb3311bf581b
im4nu <victorx811@gmail.com>
2023-07-28 10:21:00 -0300

    added toor modals

--- src/layouts/Tab.tsx
+++ src/layouts/Tab.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
+import { ModalToor } from "@/components/toor/Modal";
 import { ReactNode } from "react";
 
 interface TabLayoutProps {
@@ -8,13 +9,15 @@ interface TabLayoutProps {
 
 export default function TabLayout({ children }: TabLayoutProps) {
   return (
-    <main className="flex min-h-screen min-w-screen flex-row justify-between">
+    <main className="flex min-h-screen min-w-screen flex-row justify-between relative">
       <Sidebar />
 
       <div className="flex flex-col w-[calc(100vw-260px)] h-screen lg:px-[2.5vw] xl:px-[5vw] 2xl:px-[10vw]">
         <Header />
         {children}
       </div>
+
+      <ModalToor />
     </main>
   );
 }
